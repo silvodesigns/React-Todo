e99fc368088c5cd3017b3fcee4a8238a519ca492
Kevin-S <kevsilvo@gmail.com>
2019-06-22 11:57:36 -0400

    TodoV1

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoForm = () => {
+    return(
+        <form>
+            <input type="text" name="item"/>
+            <button>Add to List</button>
+           
+        </form>
+    )
+}
+
+export default TodoForm;
