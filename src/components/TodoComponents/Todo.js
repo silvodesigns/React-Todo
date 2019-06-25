d693dfca4f28b574fb0256ad984fa91809a49e00
Kevin-S <kevsilvo@gmail.com>
2019-06-25 00:45:25 -0400

    styling2

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -5,7 +5,15 @@ const Todo = props =>{
 
   return(
     <div class="task">
-        <p onClick={props.completed}>{props.task.task}</p>
+        <div><img className="ribbon-icon" src="icons/ribbon.svg"/></div>
+        <div className="task-description"><p onClick={props.completed}>{props.task.task}</p></div>
+        <div><img className="edit-icon" src="icons/edit.svg"/></div>
+        <div><img className="delete-icon" src="icons/delete.svg"/></div>
+
+       
+
+       
+
     </div>
   );
 };
