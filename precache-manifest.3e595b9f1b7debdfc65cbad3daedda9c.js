ac8f575e63e01e4366ed3d9f5ee735d161ad9db6
silvodesigns <kevsilvo@gmail.com>
2019-10-07 01:53:47 -0400

    Updates

---
+++ precache-manifest.3e595b9f1b7debdfc65cbad3daedda9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7baf248f1ecdb9f07662ff76a75901",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "35dfadba71c3a8ddd739",
+    "url": "/React-Todo/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "09af55ca929d774f6d61",
+    "url": "/React-Todo/static/css/main.77b35ed7.chunk.css"
+  },
+  {
+    "revision": "35dfadba71c3a8ddd739",
+    "url": "/React-Todo/static/js/2.44361f6f.chunk.js"
+  },
+  {
+    "revision": "09af55ca929d774f6d61",
+    "url": "/React-Todo/static/js/main.8f4d9f38.chunk.js"
+  },
+  {
+    "revision": "8cccfb14f8793be292ed",
+    "url": "/React-Todo/static/js/runtime~main.58a39a8a.js"
+  }
+]);
